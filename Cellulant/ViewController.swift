ae6747fc76d52ce8150ecd177898a51412621dca
Adie Olalekan <ugbeadie@gmail.com>
2020-01-21 10:48:43 +0100

    Initial Commit

---
+++ Cellulant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cellulant
+//
+//  Created by Olar's Mac on 1/21/20.
+//  Copyright © 2020 Adie Olalekan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
